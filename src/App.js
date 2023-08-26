1deb10afa54747d27be240075bfc804154708e6b
mark <cheapshoterr@gmail.com>
2023-08-26 20:32:31 +0300

    create counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 
+import { useState } from 'react';
+
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  const handleMinusCounter = ()=> {
+    setCounter(counter - 1);
+  }
+
+  const handlePlusCounter = ()=> {
+    setCounter(counter + 1);
+  }
+
   return (
     <div className="App">
-      
+      <h2>Counter:</h2>
+      <h1>{counter}</h1>
+      <button onClick={handleMinusCounter} className="minus">- Minus</button>
+      <button onClick={handlePlusCounter} className="plus">Plus +</button>
     </div>
   );
 }
